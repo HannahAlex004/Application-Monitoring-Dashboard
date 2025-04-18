ceaa4ffd87421b3f07892529b5dd7a1ba74b9601
Hannah Thankam Alex <hannahalex234@gmail.com>
2025-04-18 10:01:54 +0530

    Update load_test.py

--- load_test.py
+++ load_test.py
@@ -3,20 +3,40 @@ import time
 import random 
  
 API_BASE = "http://localhost:8000" 
-ENDPOINTS = [ 
+GET_ENDPOINTS = [ 
     "/", 
     "/users/1", 
     "/products/101", 
     "/slow-endpoint", 
-    "/error-test" 
+    "/error-test",
 ] 
+
+POST_ENDPOINTS = ["/users",]
+
+
  
 def simulate_traffic(): 
     while True: 
-        endpoint = random.choice(ENDPOINTS) 
+        method=random.choice(["GET", "POST"])
+        if method == "GET": 
+            endpoint = random.choice(GET_ENDPOINTS) 
+            try: 
+                response = requests.get(f"{API_BASE}{endpoint}") 
+                print(f"Hit {endpoint} â†’ Status: {response.status_code}") 
+            except Exception as e: 
+                print(f"Error on {endpoint}: {e}") 
+        else: 
+            endpoint = random.choice(GET_ENDPOINTS) 
+            data = {"name": f"User{random.randint(1, 100)}"} 
+            try: 
+                response = requests.post(f"{API_BASE}{endpoint}", json=data) 
+                print(f"Hit {endpoint} â†’ Status: {response.status_code}") 
+            except Exception as e: 
+                print(f"Error on {endpoint}: {e}")
+        endpoint = random.choice(POST_ENDPOINTS) 
         try: 
             response = requests.get(f"{API_BASE}{endpoint}") 
-            print(f"Hit {endpoint} → Status: {response.status_code}") 
+            print(f"Hit {endpoint} â†’ Status: {response.status_code}") 
         except Exception as e: 
             print(f"Error on {endpoint}: {e}") 
         time.sleep(random.uniform(0.1, 1.0)) 
